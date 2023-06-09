c8e4b6131db1d82a795218987f4e82ec6ad1d650
abdelhamid fekih <fekih.abdelhamid15@gmail.com>
2023-06-09 12:33:50 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+db.contactlist.insertOne({
+    "Last name": "Ben Lahmer",
+    "First name": "Fares",
+    "Email": "fares@gmail.com",
+    "age": 26
+  })
+  
+  db.contactlist.insertOne({
+    "Last name": "Kefi",
+    "First name": "Seif",
+    "Email": "kefi@gmail.com",
+    "age": 15
+  })
+  
+  db.contactlist.insertOne({
+    "Last name": "Fatnassi",
+    "First name": "Sarra",
+    "Email": "sarra.f@gmail.com",
+    "age": 40
+  })
+  
+  db.contactlist.insertOne({
+    "Last name": "Ben Yahia",
+    "First name": "Rym",
+    "age": 4
+  })
+  
+  db.contactlist.insertOne({
+    "Last name": "Cherif",
+    "First name": "Sami",
+    "age": 3
+  })
+  
